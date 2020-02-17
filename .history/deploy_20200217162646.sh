ab82496f41db106cd096c26179f27eac87010dc5
jakob <jakobeha@gmail.com>
2020-02-17 16:28:45 +0100

    Publish.

---
+++ .history/deploy_20200217162646.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# From https://jaspervdj.be/hakyll/tutorials/github-pages-tutorial.html
+
+# Temporarily store uncommited changes
+git stash
+
+# Verify correct branch
+git checkout develop
+
+# Build new files
+cabal exec site clean
+cabal exec site build
+
+# Get previous files
+git fetch --all
+git checkout -b master --track origin/master
+
+# Overwrite existing files with new files
+mkdir ../jakobeha-website-build
+cp -r .git ../jakobeha-website-build
+cp -r _site ../jakobeha-website-build
+rm -rf ./*
+mv ../jakobeha-website-build/.git .
+mv ../jakobeha-website-build/_site/ .
+
+# Commit
+git add -A
+git commit -m "Publish."
+
+# Push
+git push origin master:master
+
+# Restoration
+git checkout develop
+git branch -D master
+git stash pop
